a72b06c09ce9d44ce3c26e9314608b32e0209156
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-28 19:59:42 +0800

    add etcdRead.py

---
+++ etcdRead.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import etcd
+import sys
+import time
+import os
+import re
+reload (sys)
+def getValue(host,port,location):
+    etcdCli=etcd.Client(host=host,port=port)
+    context=etcdCli.read(location).value
+    return context
+
+getValue(host=sys.argv[1],port=int(sys.argv[2]),location=sys.argv[3])
