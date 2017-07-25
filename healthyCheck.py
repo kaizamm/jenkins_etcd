7ed544972efddd9189e99de4a827de296f241478
kaiz <KaiZ@quark.com>
2017-07-25 16:44:12 +0800

    add healthyCheck timeout

--- healthyCheck.py
+++ healthyCheck.py
@@ -4,12 +4,14 @@ import requests,sys,os
 def healthyCheck():
   if len(sys.argv) < 2: sys.exit("need 1 argument")
   url = sys.argv[1]
-  code = requests.get(url).status_code
-  if code >= 200 and code < 400:
-    print "healthy check ok , status_code:%s" % code
-  else:
-    print "healthy check not ok, status_code:%s" % code
-    sys.exit("healthy check not ok")
-
+  try:
+    code = requests.get(url,timeout=1).status_code
+    if code >= 200 and code < 400:
+      print "healthy check ok , status_code:%s" % code
+    else:
+      print "healthy check not ok, status_code:%s" % code
+      sys.exit("healthy check not ok")
+  except ReadTimeout:
+    print "HealthyCheckTimeout"
 if __name__ == "__main__":
   healthyCheck()
